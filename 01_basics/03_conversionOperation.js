9603aaa6707e3426bbd7404ffa01c29e55971a24
manishshukla0018 <manishshukla32087@gmail.com>
2025-06-14 21:45:52 +0000

    conversion Datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+ let score = null
+
+ console.log(typeof score)
+
+//  console.log(typeofscore)
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueINNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "hitesh" = true
+
+
