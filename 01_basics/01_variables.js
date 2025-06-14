7d6889641a7964b0b20b76c24fa43f9d133836a6
manishshukla0018 <manishshukla32087@gmail.com>
2025-06-14 20:45:44 +0000

    Variables And DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 453321
+let accountEmail = "manish@123"
+var accountPassword = "123456"
+
+accountCity = "Kanpur"
+// accountId = 2 // not allowed in js
+
+accountEmail = "akash@32087.com" //Allowed in JS
+
+accountPassword = "34567"
+accountCity = "Bengaluru"
+
+console.log(accountPassword)
+console.log(accountEmail)
+console.log(accountCity)
+
+/*
+prefer not to use Var because of issue in
+block scope and functional scope
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as 
+//newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+let name = "Manish"
+
+let age = 18
+
+let isLoggedIn = false
+
+// data types =>
+
+// number => 2 to pow(2,53)
+//bigint 
+//String => " "
+// boolean => True or False
+//null => standalone value 
+//undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "manish");
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // Object
