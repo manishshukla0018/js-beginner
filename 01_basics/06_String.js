56f1655cf6308b77e72e7b6e965d66b34c4ace6d
manishshukla0018 <manishshukla32087@gmail.com>
2025-06-15 08:33:35 +0000

    String Basics

---
+++ 01_basics/06_String.js
@@ -0,0 +1,36 @@
+const name = "hitesh"
+const repocount = 50
+
+// console.log(name + repocount + " value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+const getName = new String('Manish-Shukla')
+
+// console.log(getName[0])
+// console.log(getName.__proto__)
+
+
+// console.log(getName.length)
+
+// console.log(getName.toUpperCase())
+
+console.log(getName.charAt(3))
+
+console.log(getName.indexOf('i'))
+
+const newString = getName.substring(0,5)
+console.log(newString)
+
+const anotherString = getName.slice(0,5)
+console.log(anotherString)
+
+const newString1 = "  manish  "
+console.log(newString1)
+console.log(newString1.trim())
+
+const url = "htttps://manish.com/hitesh%20choudhray"
+
+console.log(url.replace("%20","-"))
+console.log(url)
+
