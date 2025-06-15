9923ef03c8f2373366689bc9ea2e86e8f05e5bb0
manishshukla0018 <manishshukla32087@gmail.com>
2025-06-15 19:13:03 +0000

    Arrays In JavaScript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array 
+ const arr = [0, 3, 5, 7, 8]
+//  console.log(arr[1])
+
+ // Array Methods
+ 
+//  arr.push(6)
+//  console.log(arr)
+//  arr.pop()
+//  console.log(arr)
+
+
+// // arr.unshift(9)
+// arr.shift()
+
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(5))
+
+// const newArr = arr.join()
+
+// console.log(arr)
+// console.log(newArr)
+
+// Slice And Splice 
+
+// console.log("A ",arr)
+
+// const my = arr.slice(1,3)
+// console.log(my)
+
+// console.log("B ",arr)
+
+// const my2 = arr.splice(1,3)
+// console.log("C ",arr)
+// console.log(my2)
+
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel = ["thor", "ironman","spiderman"]
+const dc = ["superman","flash","batman"]
+
+// marvel.push(dc)
+// console.log(marvel)
+// console.log(marvel[3])
+
+// const heroes = marvel.concat(dc)
+// console.log(heroes);
+
+// const heroes1 = [...marvel,...dc]
+// console.log(heroes1) 
+
+// const arr2 = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+// const real_arr = arr2.flat(Infinity)
+// console.log(real_arr)
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
